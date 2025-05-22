ec6769928d0c8660ac75cf52352ce5b69366e215
TymkaTy <faaairy3@gmail.com>
2025-05-22 16:04:01 +0400

    DEBUG TOOLBAR

--- Product/app/settings.py
+++ Product/app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import STATICFILES_DIRS
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,7 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "debug_toolbar",
+
     'main',
+    'goods',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -105,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -118,6 +125,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- Product/main/views.py
+++ Product/main/views.py
@@ -1,13 +1,20 @@
+import re
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories 
+
 def index(request):
+
+    categories = Categories.objects.all() 
+
     context = {
-        'title': 'home',
-        'content': 'Главная страничка'
+        'title': 'Home - Главная',
+        'content': "Ювелирый магазин Efremova",
+        'categories': categories
     } 
     
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('dasda')
+    return render(request, 'main/about.html')
