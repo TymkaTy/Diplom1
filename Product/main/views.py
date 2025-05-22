9580ba3d3e5dfcc51b108191bbc46c91967c78f4
TymkaTy <faaairy3@gmail.com>
2025-05-22 18:18:53 +0400

    Тэг и категории

--- Product/main/views.py
+++ Product/main/views.py
@@ -6,12 +6,9 @@ from goods.models import Categories
 
 def index(request):
 
-    categories = Categories.objects.all() 
-
     context = {
         'title': 'Home - Главная',
         'content': "Ювелирый магазин Efremova",
-        'categories': categories
     } 
     
     return render(request, 'main/index.html', context)
