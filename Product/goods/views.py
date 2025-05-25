4aba019fc8cd6bea3a8a60bdc7104b3007e6818b
TymkaTy <faaairy3@gmail.com>
2025-05-25 14:41:04 +0400

    Первая настройка пагинации

--- Product/goods/views.py
+++ Product/goods/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_list_or_404, get_object_or_404, render
 from goods.models import Products
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug, page=1):
     
     if category_slug == 'all':
         goods = Products.objects.all()
@@ -13,11 +13,12 @@ def catalog(request, category_slug):
         
     paginator = Paginator(goods, 3)
 
-    current_page = paginator.page(1)
+    current_page = paginator.page(page)
 
     context = {
         "title": "Home-Каталог",
         'goods': current_page, 
+        "slug_url": category_slug,
     }
     return render(request, "goods/catalog.html", context)
 
