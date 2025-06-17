7cce5187afe46ce1636e41dc1a0caf2cc44ad779
TymkaTy <faaairy3@gmail.com>
2025-06-18 00:35:20 +0400

    Классы представления

--- Product/main/views.py
+++ Product/main/views.py
@@ -1,17 +1,40 @@
 import re
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from goods.models import Categories 
 
-def index(request):
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
 
-    context = {
-        'title': 'Home - Главная',
-        'content': "Ювелирый магазин Efremova",
-    } 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - Главная'
+        context['content'] = "Магазин мебели HOME"
+        return context
+
+class AboutView(TemplateView):
+    template_name = 'main/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - О нас'
+        context['content'] = "О нас"
+        context['text_on_page'] = "Текст о том почему этот магазин такой классный, и какой хороший товар."
+        return context
     
-    return render(request, 'main/index.html', context)
 
-def about(request):
-    return render(request, 'main/about.html')
+
+
+# def index(request):
+
+#     context = {
+#         'title': 'Home - Главная',
+#         'content': "Ювелирый магазин Efremova",
+#     } 
+    
+#     return render(request, 'main/index.html', context)
+
+# def about(request):
+#     return render(request, 'main/about.html')
